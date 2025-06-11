45265438406e4ad4b285741867376779fbc0fceb
Putri Maharani <maharani2005.putrii@gmail.com>
2025-06-12 06:50:59 +0700

    Menangani gambar gagal di-load

--- app/src/main/java/com/putrimaharani0087/miniproject3/ui/screen/MainScreen.kt
+++ app/src/main/java/com/putrimaharani0087/miniproject3/ui/screen/MainScreen.kt
@@ -84,6 +84,7 @@ fun ListItem(makanan: Makanan) {
             contentDescription = stringResource(R.string.gambar, makanan.nama),
             contentScale = ContentScale.Crop,
             placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.baseline_broken_image_24),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column (
